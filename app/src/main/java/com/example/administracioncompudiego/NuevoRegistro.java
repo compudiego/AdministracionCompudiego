12eed60ef398185356fb6ed7e63ef58c9945fcf9
Luparello <compu.diego94@gmail.com>
2020-09-26 02:07:46 -0300

    primeros toques de vistas

--- app/src/main/java/com/example/administracioncompudiego/NuevoRegistro.java
+++ app/src/main/java/com/example/administracioncompudiego/NuevoRegistro.java
@@ -53,8 +53,8 @@ public class NuevoRegistro  extends AppCompatActivity {
         dniT = (EditText)findViewById(R.id.dni);
 
         mDialog = new SpotsDialog.Builder().setContext(NuevoRegistro.this).setMessage("Registrando entrada").setCancelable(false).build();
-
         guardar = (Button)findViewById(R.id.guardar);
+
         db.collection("ID").document("numeros").get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
@@ -68,110 +68,106 @@ public class NuevoRegistro  extends AppCompatActivity {
         });
 
         guardar.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
                 mDialog.show();
-
-                nombre = nombreT.getText().toString();
-                equipo = equipoT.getText().toString();
-                marca = marcaT.getText().toString();
-                modelo = modeloT.getText().toString();
-                telefono = telefonoT.getText().toString();
-                falla = fallaT.getText().toString();
-                datos = datosT.getText().toString();
-                contraseña = contraseñaT.getText().toString();
-                observaciones = observacionesT.getText().toString();
-                sn = snT.getText().toString();
-                dni = dniT.getText().toString();
-
-                String fechita = getFechaActual();
-
-                Map<String, Object> map = new HashMap<>();
-                map.put("nombre", nombre);
-                map.put("equipo", equipo);
-                map.put("marca", marca);
-                map.put("modelo", modelo);
-                map.put("telefono", telefono);
-                map.put("falla", falla);
-                map.put("id", num);
-                map.put("datos", datos);
-                map.put("contraseña",contraseña);
-                map.put("observaciones",observaciones);
-                map.put("sn",sn);
-                map.put("DNI",dni);
-                map.put("fecha", fechita);
-                db.collection("Equipos").document(num).set(map)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Log.d("TAG", "DocumentSnapshot successfully written!");
-                                mDialog.dismiss();
-
-                                AlertDialog alertDialog = new AlertDialog.Builder(NuevoRegistro.this).create();
-                                alertDialog.setTitle("Nueva Entrada");
-                                alertDialog.setMessage("Entrada Registrada con el id: " + num);
-                                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                                        new DialogInterface.OnClickListener() {
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                Intent intent1 = new Intent (getApplicationContext(), MainActivity.class);
-                                                startActivityForResult(intent1, 1);
-                                                dialog.dismiss();
-                                            }
-                                        });
-
-                                alertDialog.show();
-
-
-
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                mDialog.dismiss();
-                                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-
-                                Log.w("TAG", "Error writing document", e);
-                            }
-                        });
-                int numEntero = Integer.parseInt(num);
-                numEntero+=1;
-                String numCadena= Integer.toString(numEntero);
-                Map<String, Object> map2 = new HashMap<>();
-                map2.put("num", numCadena);
-                db.collection("ID").document("numeros").set(map2)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Log.d("TAG", "DocumentSnapshot successfully written!");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                mDialog.dismiss();
-                                Toast.makeText(getApplicationContext(), "Error2", Toast.LENGTH_SHORT).show();
-
-                                Log.w("TAG", "Error writing document", e);
-                            }
-                        });
-
-
+                guardarNuevoRegistro();
             }
-
         });
-
     }
+
+    private void guardarNuevoRegistro() {
+
+        nombre = nombreT.getText().toString();
+        equipo = equipoT.getText().toString();
+        marca = marcaT.getText().toString();
+        modelo = modeloT.getText().toString();
+        telefono = telefonoT.getText().toString();
+        falla = fallaT.getText().toString();
+        datos = datosT.getText().toString();
+        contraseña = contraseñaT.getText().toString();
+        observaciones = observacionesT.getText().toString();
+        sn = snT.getText().toString();
+        dni = dniT.getText().toString();
+
+        String fechita = getFechaActual();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("nombre", nombre);
+        map.put("equipo", equipo);
+        map.put("marca", marca);
+        map.put("modelo", modelo);
+        map.put("telefono", telefono);
+        map.put("falla", falla);
+        map.put("id", num);
+        map.put("datos", datos);
+        map.put("contraseña",contraseña);
+        map.put("observaciones",observaciones);
+        map.put("sn",sn);
+        map.put("DNI",dni);
+        map.put("fecha", fechita);
+
+        db.collection("Equipos").document(num).set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("TAG", "DocumentSnapshot successfully written!");
+                        mDialog.dismiss();
+
+                        AlertDialog alertDialog = new AlertDialog.Builder(NuevoRegistro.this).create();
+                        alertDialog.setTitle("Nueva Entrada");
+                        alertDialog.setMessage("Entrada Registrada con el id: " + num);
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent1 = new Intent (getApplicationContext(), MainActivity.class);
+                                        startActivityForResult(intent1, 1);
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        mDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                        Log.w("TAG", "Error writing document", e);
+                    }
+                });
+
+        sumarId();
+    }
+
+    private void sumarId() {
+        int numEntero = Integer.parseInt(num);
+        numEntero+=1;
+        String numCadena= Integer.toString(numEntero);
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("num", numCadena);
+        db.collection("ID").document("numeros").set(map2)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("TAG", "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        mDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Error2", Toast.LENGTH_SHORT).show();
+
+                        Log.w("TAG", "Error writing document", e);
+                    }
+                });
+    }
+
     public static String getFechaActual() {
         Date ahora = new Date();
         SimpleDateFormat formateador = new SimpleDateFormat("dd-MM-yyyy");
         return formateador.format(ahora);
     }
-
-
-
-
-
 
 }
